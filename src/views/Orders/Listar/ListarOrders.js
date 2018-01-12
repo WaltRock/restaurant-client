5ec245c6a80a3c64aeb62daefe51ceb6bf432b26
Walter Quispe Rios <Walt_2007.0@hotmail.com>
2018-01-12 15:15:13 -0500

    Listado de ordenes

--- src/views/Orders/Listar/ListarOrders.js
+++ src/views/Orders/Listar/ListarOrders.js
@@ -11,21 +11,30 @@ import {
   CardBody,
   Label,
   Input,
-  Button
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  InputGroup
 } from 'reactstrap';
-
-function editarPlato(e) {
-  console.log(e)
-}
 
 class ListarOrders extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      tipoPagoSeleccionado: '',
       listaOrdenes: [],
-      tiposEstado: []
+      tiposEstado: [],
+      tiposPago: [],
+      ordenSeleccionada: {
+        details: []
+      }
     }
-    this.BotonesCache = {}
+
+    //Handlers
     this.handleChange = this
       .handleChange
       .bind(this)
@@ -35,12 +44,39 @@ class ListarOrders extends Component {
     this.CardOrden = this
       .CardOrden
       .bind(this)
+    this.botonHeaderClick = this
+      .botonHeaderClick
+      .bind(this)
+    this.toggleSuccess = this
+      .toggleSuccess
+      .bind(this)
+    this.actualizarPedido = this
+      .actualizarPedido
+      .bind(this)
+    this.ListaPlatos = this
+      .ListaPlatos
+      .bind(this)
+    this.getParamsOrder = this
+      .getParamsOrder
+      .bind(this)
+    this.getFechaString = this
+      .getFechaString
+      .bind(this)
+
+    //Axios
     let listaPromesas = [
       axios.get('EstadoOrdenes'),
       axios.get('Ordenes/getOrderDetails')
     ]
+    axios
+      .get('TiposDePago')
+      .then(response => {
+        this.state.tiposPago = response.data
+      })
 
-    this.getBotonoesProceso = this.getBotonoesProceso.bind(this)
+    this.getBotonesProceso = this
+      .getBotonesProceso
+      .bind(this)
 
     Promise
       .all(listaPromesas)
@@ -51,38 +87,158 @@ class ListarOrders extends Component {
   }
 
   handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+    this.setState({tipoPagoSeleccionado: e.target.value})
   }
 
-  getBotonoesProceso(EstadoOrdenId) {
-    let listaBotones = []
-    if (!this.BotonesCache[EstadoOrdenId]) {
-      this.BotonesCache[EstadoOrdenId] = []
-      
+  toggleSuccess(esTerminado) {
+    if (esTerminado && esTerminado.target.name === 'pagar') {
+      this.setState({esPago: false})
+      let params = this.getParamsOrder(this.state.ordenSeleccionada)
       this
         .state
-        .tiposEstado
-        .forEach(estado => {
-          if (estado.id !== EstadoOrdenId) {
-            this
-              .BotonesCache[EstadoOrdenId]
-              .push(<Button key={estado.id} className={classNames({
-                [estado.color]: true,
-                'float-right': true
-              })} type="button" color="primary">{estado.descripcion}</Button>)
+        .listaOrdenes
+        .forEach((orden) => {
+          if (orden.id === this.state.ordenSeleccionada.id) {
+            orden.EstadoOrdenId = this.state.ordenSeleccionada.EstadoOrdenIdTmp
+            this.forceUpdate()
           }
         })
-        
+      this.actualizarPedido(params)
+    }
+    this.setState({
+      success: !this.state.success
+    });
+  }
+
+  ListaTiposPago() {
+    return this
+      .state
+      .tiposPago
+      .map(tipoPago => (
+        <div key={tipoPago.id} className="radio">
+          <Label check htmlFor="radio2">
+            <Input
+              type="radio"
+              name="radios"
+              value={tipoPago.id}
+              checked={this.state.tipoPagoSeleccionado === tipoPago.id}
+              onChange={this.handleChange}/> {tipoPago.descripcion}
+          </Label>
+        </div>
+      ))
+
+  }
+
+  actualizarPedido(params) {
+    const url = 'Ordenes/' + params.id
+    axios
+      .put(url, params)
+      .then(response => {})
+      .catch(err => {});
+  }
+
+  getFechaString(orden) {
+    var f = new Date(orden.Fecha),
+      mes = f.getMonth() + 1,
+      dia = f.getDate(),
+      hora = f.getHours(),
+      minutos = f.getMinutes(),
+      segundos = f.getSeconds();
+
+    function anadecero(valor) {
+      if (valor < 10) 
+        valor = '0' + valor;
+      return valor
+    }
+
+    return f.getFullYear() + "-" + anadecero(mes) + "-" + anadecero(dia) + " " + anadecero(hora) + ":" + anadecero(minutos) + ":" + anadecero(segundos)
+
+  }
+  getParamsOrder(orden, estado) {
+    let EstadoOrdenId = orden.EstadoOrdenIdTmp
+
+    if (!EstadoOrdenId) {
+      EstadoOrdenId = (estado && estado.id)
+        ? estado.id
+        : orden.EstadoOrdenId
     } 
-    return this.BotonesCache[EstadoOrdenId]
+
+    let params = {
+      "id": orden.id,
+      "EstadoOrdenId": EstadoOrdenId,
+      "Fecha": orden.Fecha,
+      "TipoPagoId": orden.TipoPagoId,
+      "NombreCliente": orden.NombreCliente,
+      "CostoTotal": orden.CostoTotal
+    }
+    if (this.state.tipoPagoSeleccionado.length > 0) {
+      params.TipoPagoId = this.state.tipoPagoSeleccionado
+      this.state.tipoPagoSeleccionado = ''
+    }
+    return params
+  }
+
+  botonHeaderClick(orden, estado) {
+    const params = this.getParamsOrder(orden, estado)
+    if (estado.descripcion === 'Terminado' || estado.descripcion === 'ver') {
+      const fecha = this.getFechaString(orden)
+
+      this.state.ordenSeleccionada = JSON.parse(JSON.stringify(orden))
+      this.state.ordenSeleccionada.EstadoOrdenIdTmp = estado.id
+      this.state.tipoPagoSeleccionado =  this.state.ordenSeleccionada.TipoPagoId
+      this.state.ordenSeleccionada.fechaString = fecha;
+      console.log(estado.descripcion)
+      if(estado.descripcion === 'Terminado'){
+        this.setState({esPago: true})
+      }else{
+        this.setState({esPago: false})
+      }
+      
+      this.toggleSuccess()
+    } else {
+      orden.EstadoOrdenId = estado.id
+      this.forceUpdate()
+      this.actualizarPedido(params)
+    }
+  }
+
+  ListaPlatos(platos) {
+    return platos.map((plato, index) => (
+      <li key={index} className="list-group-item">
+        <span>{plato.DetallePlato.nombre}</span>
+        <span className="float-right">{parseFloat(plato.DetallePlato.costo).toFixed(2)}</span>
+      </li>
+    ))
+  }
+
+  getBotonesProceso(EstadoOrdenId, orden) {
+    let listaBotones = []
+    this
+      .state
+      .tiposEstado
+      .forEach(estado => {
+        if (estado.id !== EstadoOrdenId) {
+          listaBotones.push(
+            <Button
+              key={estado.id}
+              className={classNames({
+              [estado.color]: true,
+              'float-right': true
+            })}
+              type="button"
+              onClick={() => {
+              this.botonHeaderClick(orden, estado)
+            }}
+              color="primary">{estado.descripcion}</Button>
+          )
+        }
+      })
+
+    return listaBotones
   }
 
   CardOrden(EstadoOrdenId) {
-
     let listaCards = []
-    const BotonesHeader = this.getBotonoesProceso(EstadoOrdenId)
     this
       .state
       .listaOrdenes
@@ -92,10 +248,10 @@ class ListarOrders extends Component {
             <Card key={orden.id} md="12">
               <CardHeader>
                 {orden.NombreCliente}
-                {BotonesHeader}
+                {this.getBotonesProceso(EstadoOrdenId, orden)}
               </CardHeader>
               <CardBody>
-                <Badge pill className="float-right badge-info">{orden.details.length}</Badge>
+                <Badge pill onClick={()=>{this.botonHeaderClick(orden,{descripcion:'ver'})}}  className="float-right badge-info">{orden.details.length}</Badge>
                 {orden.details[0].DetallePlato.nombre}
               </CardBody>
             </Card>
@@ -126,7 +282,6 @@ class ListarOrders extends Component {
           </Col>
         )
       })
-
   }
 
   render() {
@@ -135,6 +290,74 @@ class ListarOrders extends Component {
         <Row>
           {this.ColumnaEstadoOrden()}
         </Row>
+        <Modal
+          isOpen={this.state.success}
+          toggle={this.toggleSuccess}
+          className={'modal-success ' + this.props.className}>
+          <ModalHeader toggle={this.toggleSuccess}>Pagos</ModalHeader>
+          <ModalBody>
+            <Form className="row">
+              <div className="col-md-12">
+                <span className="float-right">{'Hora de pedido ' + this.state.ordenSeleccionada.fechaString}</span>
+              </div>
+              <FormGroup className="col-md-12">
+                <Label htmlFor="prependedInput">Nombre</Label>
+                <div className="controls">
+                  <InputGroup className="input-prepend">
+                    <Input
+                      name="nombreCliente"
+                      value={this.state.ordenSeleccionada.NombreCliente}
+                      disabled
+                      type="text"/>
+                  </InputGroup>
+                </div>
+              </FormGroup>
+
+              <FormGroup className="col-md-12">
+                <Label htmlFor="prependedInput">Costo Total</Label>
+                <div className="controls">
+                  <InputGroup className="input-prepend">
+                    <Input
+                      name="nombreCliente"
+                      value={parseFloat(this.state.ordenSeleccionada.CostoTotal).toFixed(2)}
+                      disabled
+                      type="text"/>
+                  </InputGroup>
+                </div>
+              </FormGroup>
+              <FormGroup className="col-md-12">
+                <Row>
+                  <Col md="3">
+                    <Label>Tipo de pago</Label>
+                  </Col>
+                  <Col md="9">
+                    <FormGroup check>
+                      {this.ListaTiposPago()}
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </FormGroup>
+              <FormGroup className="col-md-12">
+                <Label htmlFor="prependedInput">Consumo</Label>
+                <div className="controls">
+                  <ul className="list-group">
+                    {this.ListaPlatos(this.state.ordenSeleccionada.details)}
+                  </ul>
+                </div>
+              </FormGroup>
+
+            </Form>
+
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              name="pagar"
+              className={classNames({'d-none': !this.state.esPago})}
+              color="success"
+              onClick={this.toggleSuccess}>Pagar</Button>{' '}
+            <Button color="secondary" onClick={this.toggleSuccess}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
       </div>
     )
   }
